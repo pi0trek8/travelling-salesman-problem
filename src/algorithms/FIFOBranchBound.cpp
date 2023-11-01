a32af7b746cf8e97eb3cfba29a1a181ec2a35794
Pi0trek8 <piotrekszczypior1@gmail.com>
2023-11-01 13:42:50 +0100

    test data

--- src/algorithms/FIFOBranchBound.cpp
+++ src/algorithms/FIFOBranchBound.cpp
@@ -6,35 +6,41 @@ AlgorithmResultTO *FIFOBranchBound::process(Graph *graph) {
     int city_number = graph->get_city_number();
     auto matrix = mapToVector(graph);
 
-    std::queue<Matrix *> queue;
+    List<Matrix *> queue;
     auto parent_node = new Matrix(nullptr, 0, vector<bool>(city_number, false), matrix);
     parent_node->perform_first_reduction();
-    queue.push(parent_node);
+    queue.push_back(parent_node);
 
     Matrix *lower_node = nullptr;
 
     int upper_bound = INT_MAX;
+    bool is_completed;
 
-    while (!queue.empty()) {
-        parent_node = queue.front();
-        queue.pop();
+    while (!queue.get_size()) {
+        parent_node = queue[0];
+        queue.pop_front();
         auto visited_cities = parent_node->get_visited_cities();
         visited_cities[parent_node->get_city()] = true;
 
+        if (upper_bound < parent_node->getCost()) {
+            delete parent_node;
+            continue;
+        }
+
+        is_completed = true;
         for (int city = 0; city < city_number; ++city) {
             if (visited_cities[city]) {
                 continue;
             }
+            is_completed = false;
             auto new_node = new Matrix(parent_node, city, visited_cities, parent_node->get_matrix());
             new_node->reduce_matrix(parent_node->get_city(), city, parent_node->getCost());
+            queue.push_front(new_node);
+        }
 
-            if (upper_bound > new_node->getCost()) {
-                upper_bound = new_node->getCost();
-                queue.push(new_node);
-                lower_node = new_node;
-            } else {
-                delete new_node;
-            }
+        if (is_completed && upper_bound > parent_node->getCost()) {
+            lower_node = parent_node;
+            upper_bound = parent_node->getCost();
         }
     }
 
